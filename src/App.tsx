c03395eebcb10cd30fa7fb1035ab21b4b5be5765
Erik Larsen <Erikstelarsen@outlook.com>
2025-02-26 10:44:59 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import WordleGame from './components/WordleGame';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import TopMenuBar from './components/menu/TopMenuBar';
 import Instructions from './components/Instructions';
 import About from './components/About';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter >
+    <HashRouter >
       <div className="startPage">
         <TopMenuBar />
         <Routes>
-          <Route path="/wordgames" element={<WordleGame />} />
+          <Route path="/" element={<WordleGame />} />
           <Route path="/instructions" element={<Instructions />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
     
   );
 };
